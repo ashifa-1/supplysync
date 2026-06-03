f512885c403ae941b03702b1b915439464312ae6
Ashifa <mdashifa12@gmail.com>
2026-06-03 16:16:15 +0530

    fix app config names for nested apps package structure

--- apps/sales_orders/apps.py
+++ apps/sales_orders/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SalesOrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sales_orders'
+    name = 'apps.sales_orders'
--- apps/warehouses/apps.py
+++ apps/warehouses/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WarehousesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'warehouses'
+    name = 'apps.warehouses'
